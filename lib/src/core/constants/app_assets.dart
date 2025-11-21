4b4b8e40445ae17368663baa3816e5bc7f194758
Modather22222 <modathersalahmohamed@gmail.com>
2025-11-21 15:17:29 +0200

    Revert "feat: Add initial home and onboarding screens with associated assets and asset constants."
    This reverts commit b04b5887ad268dc0331edacb77c8bd9c9ea7ba73.

--- lib/src/core/constants/app_assets.dart
+++ lib/src/core/constants/app_assets.dart
@@ -2,10 +2,4 @@ class AppAssets {
   static const String splashLogo = 'assets/images/splash_logo.png';
   static const String onboarding1 = 'assets/images/onboarding_1.png';
   static const String onboarding2 = 'assets/images/onboarding_2.png';
-  static const String onboarding3 = 'assets/images/onboarding_3.png';
-  static const String homeBanner = 'assets/images/home_banner.png';
-  static const String catEducational = 'assets/images/cat_educational.png';
-  static const String catIslamic = 'assets/images/cat_islamic.png';
-  static const String storyThumb1 = 'assets/images/story_thumb_1.png';
-  static const String storyThumb2 = 'assets/images/story_thumb_2.png';
 }
